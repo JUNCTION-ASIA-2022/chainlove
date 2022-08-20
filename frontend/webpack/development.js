e880b158c6d2e2d48a9dd9ba57d57f26131899de
Millie <jay.lee.2280@gmail.com>
2022-08-20 20:44:24 +0900

    Init: setting with webpack

---
+++ frontend/webpack/development.js
@@ -0,0 +1,39 @@
+const path = require('path');
+
+const webpack = require('webpack');
+
+const PORT = 9000;
+
+module.exports = {
+  mode: 'development',
+
+  devtool: 'eval-source-map',
+
+  module: {
+    rules: [
+      {
+        test: /\.(sc|c|sa)ss$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+
+  plugins: [new webpack.ProgressPlugin()],
+
+  devServer: {
+    client: {
+      overlay: {
+        errors: true,
+        warnings: false,
+      },
+      progress: true,
+    },
+    compress: true,
+    historyApiFallback: true,
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    open: true,
+    port: PORT,
+  },
+};
